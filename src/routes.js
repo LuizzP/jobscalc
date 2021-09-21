2f19342d9e68f14f0557983a0410486e6a498f4c
LuizzP <luizz.paullo.lp@outlook.com>
2021-09-21 09:18:02 -0400

    criado variaveis atraves do EJS

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,19 @@ const routes = express.Router()
 
 const basePath = __dirname + "/views/"
 
+const profile = {
+    name : "LuizzP",
+    avatar : "https://xesque.rocketseat.dev/users/avatar/profile-66676016-1631556207095.jpg",
+    "monthly-budget": 5000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
+
 //req, res
 routes.get('/', (req, res) => res.render(basePath + "index"))
 routes.get('/job', (req, res) => res.render(basePath + "job"))
 routes.get('/job/edit', (req, res) => res.render(basePath + "job-edit"))
-routes.get('/profile', (req, res) => res.render(basePath + "profile"))
+routes.get('/profile', (req, res) => res.render(basePath + "profile", {profile}))
 
 module.exports = routes;
